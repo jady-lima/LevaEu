a6bc7124f28b73804ce956599686f0836349604b
Jady Lima <jadylsilva@gmail.com>
2024-05-10 20:28:22 -0300

    Update registrationCar.dart

--- levaeu_mobile/lib/screens/registrationCar.dart
+++ levaeu_mobile/lib/screens/registrationCar.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:levaeu_mobile/utils/titles_screens.dart';
 
 class RegistrationCar extends StatefulWidget{
   @override
@@ -8,6 +9,8 @@ class RegistrationCar extends StatefulWidget{
 }
 
 class _RegistrationCarState extends State<RegistrationCar> {
+
+  final _formKeyRegistration = GlobalKey<FormState>();
 
  Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +22,40 @@ class _RegistrationCarState extends State<RegistrationCar> {
         iconTheme: const IconThemeData(color: Color.fromRGBO(57, 96, 143, 1.0)),
       ),
 
-      body: Text("Registro carro"),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              
+              Container(
+                width: 120,
+                height: 120,
+                child: const Icon(Icons.drive_eta_rounded, color: Color.fromRGBO(57, 96, 143, 1.0), size: 100,)
+              ),
+
+              Container(
+                margin: const EdgeInsets.only( bottom: 10),
+                constraints: const BoxConstraints(maxWidth: 350),
+                child: TitlesScreens.buildMainTitle("Informações do Veículo")
+              ),
+
+              Container(
+                constraints: const BoxConstraints(maxWidth: 250),
+                child: TitlesScreens.buildSecondaryTitle("Por favor, nos informe os dados do seu veículo para finalizar a criação da sua conta")
+              ),
+
+              Form(
+                key: _formKeyRegistration,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget> []
+                )
+              )
+            ]
+          )
+        ]
+      )
     );
   }
 
