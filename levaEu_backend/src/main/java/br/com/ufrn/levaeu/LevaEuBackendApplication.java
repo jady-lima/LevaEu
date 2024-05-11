7088c894c86249cbe223e4ecd77a20e85cf9b69b
carlos <carlostertuliano07@gmail.com>
2024-05-11 12:00:17 -0300

    feat: criação projeto backend e configuração do application.properties

---
+++ levaEu_backend/src/main/java/br/com/ufrn/levaeu/LevaEuBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.ufrn.levaeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevaEuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LevaEuBackendApplication.class, args);
+    }
+
+}
