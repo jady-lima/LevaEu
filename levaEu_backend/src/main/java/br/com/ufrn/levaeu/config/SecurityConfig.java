0d9f7379a307e23c6e47591ec953b848d8cd0d63
carlosTertuliano <carlosTertuliano07@gmail.com>
2024-05-27 21:45:52 -0300

    feat: adiciona cadastro e login de usuário

--- levaEu_backend/src/main/java/br/com/ufrn/levaeu/config/SecurityConfig.java
+++ levaEu_backend/src/main/java/br/com/ufrn/levaeu/config/SecurityConfig.java
@@ -2,9 +2,17 @@ package br.com.ufrn.levaeu.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.server.authentication.logout.DelegatingServerLogoutHandler;
+import org.springframework.security.web.server.authentication.logout.SecurityContextServerLogoutHandler;
+import org.springframework.security.web.server.authentication.logout.WebSessionServerLogoutHandler;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -15,14 +23,25 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Desabilita CSRF
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/**").permitAll() // Permite todas as requisições para todos os caminhos
-                        .anyRequest().authenticated() // Assegura que todas as outras requisições sejam autenticadas
-                )
-                .httpBasic(withDefaults()); // Habilita autenticação básica
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                ).httpBasic(withDefaults());
 
         return http.build();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+
 }
 
---
+++ levaEu_backend/src/main/java/br/com/ufrn/levaeu/service/TokenService.java
@@ -0,0 +1,65 @@
+package br.com.ufrn.levaeu.service;
+
+import br.com.ufrn.levaeu.model.User;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withIssuer("auth-user")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(generateExpirationDate())
+                    .sign(algorithm);
+
+            return token;
+        } catch (JWTCreationException ex) {
+            throw new RuntimeException("Erro enquanto tentava gerar token");
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            token = token.trim();
+            return JWT.require(algorithm)
+                    .withIssuer("auth-user")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException ex) {
+            return "";
+        }
+    }
+
+    public String resolveToken(HttpServletRequest req) {
+        String authHeader = req.getHeader("Authorization");
+        if (authHeader == null) {
+            return null;
+        }
+
+        return authHeader.replace("Bearer", "").trim();
+    }
+
+    private Instant generateExpirationDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
