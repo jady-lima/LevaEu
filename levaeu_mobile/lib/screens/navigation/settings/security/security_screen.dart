a2508d396809f0998003b2aff82a10da878c58a9
Jady Lima <60115430+jady-lima@users.noreply.github.com>
2024-06-11 18:22:57 -0300

    Update security_screen.dart

--- levaeu_mobile/lib/screens/navigation/settings/security/security_screen.dart
+++ levaeu_mobile/lib/screens/navigation/settings/security/security_screen.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:levaeu_mobile/screens/navigation/settings/security/change_pass.dart';
-import 'package:levaeu_mobile/utils/elevated_buttons.dart';
+import 'package:levaeu_mobile/model/user_data.dart';
+import 'package:levaeu_mobile/utils/text_fields_forms.dart';
+import 'package:provider/provider.dart';
 
 class Security extends StatefulWidget {
-  const Security({super.key});
+  const Security({Key? key}) : super(key: key);
 
   @override
   SecurityState createState() => SecurityState();
 }
 
 class SecurityState extends State<Security> {
+  TextEditingController senhaAtual = TextEditingController();
+  TextEditingController novaSenha = TextEditingController();
+  final _formKeyPass = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
+    final userData = Provider.of<UserData>(context);
+
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.onInverseSurface,
 
@@ -28,34 +35,126 @@ class SecurityState extends State<Security> {
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            
-            /*
             Container(
-              constraints: const BoxConstraints(maxWidth: 380, minWidth: 200, maxHeight: 100, minHeight: 50),
-              alignment: Alignment.center,
-              margin: const EdgeInsets.only(top: 10),
-              child: ElevatedButtonsForms.buildElevatedButtonIcon(
-                Icons.lock_outline_rounded, 
-                "Autenticação de dois fatores", 
-                context, 
-                () => const Autentication(), 
-                rootNavigator: true
+              constraints: const BoxConstraints(
+                maxWidth: 380,
+                minWidth: 200,
+                maxHeight: 100,
+                minHeight: 50,
               ),
-            ),
-            */
+              alignment: Alignment.center,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(200, 80),
+                  maximumSize: const Size(320, 120),
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  side: const BorderSide(
+                    color: Color.fromRGBO(196, 198, 208, 1.0),
+                  ),
+                ),
+                child: const Row(
+                  children: <Widget>[
+                    Icon(Icons.key),
+                    Text(
+                      " Alterar senha",
+                      style: TextStyle(
+                        color: Color.fromRGBO(90, 90, 90, 1.0),
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+                onPressed: () {
+                  showModalBottomSheet<void>(
+                    context: context,
+                    isScrollControlled: true, 
+                    builder: (BuildContext context) {
+                      return Padding(
+                        padding: EdgeInsets.only(
+                          bottom: MediaQuery.of(context).viewInsets.bottom,
+                        ),
+                        child: DraggableScrollableSheet(
+                          initialChildSize: 0.35, 
+                          minChildSize: 0.2, 
+                          maxChildSize: 0.8,
+                          builder: (BuildContext context, ScrollController scrollController) {
+                            return Form(
+                              key: _formKeyPass,
+                              child: Container(
+                                color: Colors.white,
+                                padding: const EdgeInsets.all(16.0),
+                                child: SingleChildScrollView(
+                                  controller: scrollController, 
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: <Widget>[
 
-            Container(
-              constraints: const BoxConstraints(maxWidth: 380, minWidth: 200, maxHeight: 100, minHeight: 50),
-              alignment: Alignment.center,
-              child: ElevatedButtonsForms.buildElevatedButtonIcon(
-                Icons.key, 
-                "Alterar senha", 
-                context, 
-                () => const ChangePass(), 
-                rootNavigator: true
+                                      TextFormField(
+                                        obscureText: true,
+                                        controller: senhaAtual,
+                                        validator: TextFieldsForms.validatePassword,
+                                        decoration: const InputDecoration(
+                                          hintText: 'Senha atual',
+                                        ),
+                                      ),
+
+                                      const SizedBox(height: 10),
+
+                                      TextFormField(
+                                        obscureText: true,
+                                        controller: novaSenha,
+                                        validator: TextFieldsForms.validatePassword,
+                                        decoration: const InputDecoration(
+                                          hintText: 'Nova senha',
+                                        ),
+                                      ),
+
+                                      const SizedBox(height: 10),
+
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          if (userData.pass == senhaAtual.text && _formKeyPass.currentState!.validate()) {
+                                            print(userData.pass);
+                                            userData.updatePass(novaSenha.text);
+                                            print(userData.pass);
+                                            novaSenha.text = "";
+                                            senhaAtual.text = "";
+                                            Navigator.pop(context);
+                                          } else if (userData.pass != senhaAtual.text) {
+                                            ScaffoldMessenger.of(context).showSnackBar(
+                                              const SnackBar(content: Text('Senha atual incorreta')),
+                                            );
+                                          }
+                                        },
+                                        child: const Text('Alterar senha'),
+                                      ),
+
+                                      const SizedBox(height: 10),
+                                      
+                                      ElevatedButton(
+                                        child: const Text('Fechar'),
+                                        onPressed: (){ 
+                                          novaSenha.text = "";
+                                          senhaAtual.text = "";
+                                          Navigator.pop(context);
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              )
+                            );
+                          },
+                        ),
+                      );
+                    },
+                  );
+                },
               ),
-            ),
-            
+            )
           ],
         ),
       ),
