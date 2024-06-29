d779ba966e8eed898899380a8b34e2c52562a0de
carlosTertuliano <carlosTertuliano07@gmail.com>
2024-06-29 19:03:42 -0300

    refact: endereço dentro da classe usuário

--- levaEu_backend/src/main/java/br/com/ufrn/levaeu/model/User.java
+++ levaEu_backend/src/main/java/br/com/ufrn/levaeu/model/User.java
@@ -33,6 +33,69 @@ public class User implements UserDetails {
     @NotNull
     private TypeUser typeUser;
     private String gender;
+    private String cep;
+    private String street;
+    private String number;
+    private String district;
+    private String city;
+    private String state;
+    private String country;
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String rua) {
+        this.street = rua;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String numero) {
+        this.number = numero;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String bairro) {
+        this.district = bairro;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String cidade) {
+        this.city = cidade;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String estado) {
+        this.state = estado;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String pais) {
+        this.country = pais;
+    }
     
     // Getters and setters
 
